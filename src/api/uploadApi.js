00080a6328309bdd96774f7fe42b32a4023777fc
Anjana V U <103420152+anjanavu@users.noreply.github.com>
2026-03-09 19:53:37 +0530

    fix:upload api

--- src/api/uploadApi.js
+++ src/api/uploadApi.js
@@ -4,11 +4,11 @@ export const uploadFile = async (file) => {
   try {
     const formData = new FormData();
     formData.append("file", file);
+
     const response = await axiosInstance.post("/upload", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
+      skipAuthRefresh: true,
     });
+
     return response.data;
   } catch (error) {
     throw error.response?.data || error;
